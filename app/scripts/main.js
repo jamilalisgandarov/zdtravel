52fd156070cb19d70d20e0d13416eb9d83699a4b
Seymur <tagiyevseymur@hotmail.com>
2017-11-01 21:44:57 +0400

    responsivelik complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,64 @@
 $(document).ready(function() {
-  $(".tours-list").owlCarousel({
+  $('.tours-list').owlCarousel({
     items: 3,
     margin: 30,
     nav: true,
     navText: [
       '<span class="lnr lnr-arrow-left"></span>',
       '<span class="lnr lnr-arrow-right"></span>'
-    ]
+    ],
+    responsive : {
+      // breakpoint from 0 up
+      0 : {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      480 : {
+        items: 1,
+      },
+      // breakpoint from 768 up
+      768 : {
+        items: 3,
+      }
+    }
   });
 
-  $(".preview-list").slick({
+  $('.preview-list').slick({
     slidesToShow: 1,
     infinite: false,
     arrows: false,
     nextArrow: '<span class="lnr lnr-arrow-right"></span>',
     prevArrow: '<span class="lnr lnr-arrow-left"></span>',
-    appendArrows: $(".explore-previews .navs")
+    appendArrows: $('.explore-previews .navs')
   });
 
-  $(".reviews").owlCarousel({
+  $('.reviews').owlCarousel({
     items: 2,
     infinite: false,
     nav: true,
-    navText:['<i class="lnr lnr-arrow-left" aria-hidden="true"></i>','<i class="lnr lnr-arrow-right" aria-hidden="true"></i>']
+    navText:['<i class="lnr lnr-arrow-left" aria-hidden="true"></i>','<i class="lnr lnr-arrow-right" aria-hidden="true"></i>'],
+    responsive : {
+      // breakpoint from 0 up
+      0 : {
+        items: 1
+      },
+      // breakpoint from 480 up
+      480 : {
+        items: 1
+      },
+      // breakpoint from 768 up
+      768 : {
+        items: 2
+      }
+    }
   });
 
 
-  $(".explore-previews .navs>span").click(function(e) {
+  $('.explore-previews .navs>span').click(function(e) {
     var slider = $('.preview-list');
     var element = $(this);
     var elementNavToward = element.data('arrow');
-    $(".explore-previews .slick-slide.slick-active").addClass("scale-out");
+    $('.explore-previews .slick-slide.slick-active').addClass('scale-out');
     if(elementNavToward=='prev'){
       setTimeout(function() {
         $('.preview-list').slick('slickPrev');
